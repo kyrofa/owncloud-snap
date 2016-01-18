f3ad86b57d1d256d300a98b0a43ebfc847744a0b
Kyle Fazzari <kyle@canonical.com>
2016-01-18 20:02:23 +0000

    Add ownCloud part.
    This includes adding the PHP modules required by ownCloud, disabling the
    rpath on the PHP module, and adding initial setup for mysql and owncloud.
    Also patched owncloud to support an out-of-tree config. Finally, stopped
    building embedded mysql server-- it was a waste of space.
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

---
+++ src/owncloud/autoconfig.php
@@ -0,0 +1,17 @@
+<?php
+
+$data_path = '/var/lib/apps/'.getenv('SNAP_FULLNAME').'/current';
+
+$AUTOCONFIG = array(
+'directory' => $data_path.'/owncloud/data',
+
+'dbtype' => 'mysql',
+
+'dbhost' => 'localhost:'.$data_path.'/mysql/mysql.sock',
+
+'dbname' => 'owncloud',
+
+'dbuser' => 'owncloud',
+
+'dbpass' => getenv('OWNCLOUD_DATABASE_PASSWORD'),
+);
---
+++ src/owncloud/config.php
@@ -0,0 +1,32 @@
+<?php
+
+$CONFIG = array(
+/**
+ * Use the ``apps_paths`` parameter to set the location of the Apps directory,
+ * which should be scanned for available apps, and where user-specific apps
+ * should be installed from the Apps store. The ``path`` defines the absolute
+ * file system path to the app folder. The key ``url`` defines the HTTP web path
+ * to that folder, starting from the ownCloud web root. The key ``writable``
+ * indicates if a web server can write files to that folder.
+ */
+'apps_paths' => array(
+	array(
+		'path'=> '/var/lib/apps/'.getenv('SNAP_FULLNAME').'/current/owncloud/apps',
+		'url' => '/apps',
+		'writable' => true,
+	),
+),
+
+/**
+ * Database types that are supported for installation.
+ *
+ * Available:
+ * 	- sqlite (SQLite3 - Not in Enterprise Edition)
+ * 	- mysql (MySQL)
+ * 	- pgsql (PostgreSQL)
+ * 	- oci (Oracle - Enterprise Edition Only)
+ */
+'supportedDatabases' => array(
+	'mysql',
+),
+);
